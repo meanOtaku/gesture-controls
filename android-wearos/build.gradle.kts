ec3d27756ca11dcb9b37e11ab84600c5e404d320
Vaibhav Bisht <56401767+meanOtaku@users.noreply.github.com>
2026-08-23 18:22:17 +0530

    wearOS update

--- android-wearos/build.gradle.kts
+++ android-wearos/build.gradle.kts
@@ -1,5 +1,5 @@
 // Root build file for the standalone Wear OS client. No desktop code lives under this module.
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    id("com.android.application") version "8.13.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
